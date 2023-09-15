b9336725574c96efac3ed17da33e399bacfe129c
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2023-09-15 07:51:58 +0100

    temporary commit for backup reasons

--- src/jsonPathCommand.ts
+++ src/jsonPathCommand.ts
@@ -1,7 +1,9 @@
 import { TextEditor, TextEditorEdit, ViewColumn, WebviewPanel, window } from "vscode";
+import { JsonPathEditorProvider } from "./jsonPathEditor";
 
 export function openWindow(editor: TextEditor, edit: TextEditorEdit): WebviewPanel {
     const jsonPathWebView = window.createWebviewPanel('jsonPath.jsonpath', 'JsonPath-preview.jsonpath', { preserveFocus: false, viewColumn: ViewColumn.Beside }, { enableFindWidget: true, enableScripts: true, enableCommandUris: true });
+    // window.registerCustomEditorProvider('jsonPath.jsonpath', JsonPathEditorProvider.register() )
 
     return jsonPathWebView;
 }
